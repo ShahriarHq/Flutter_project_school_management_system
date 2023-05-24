8309400d3aa2eeb905980465f7ec199b29a24bb3
S.M. Shahriar Haque <shahriar@SMs-MacBook-Pro.local>
2023-05-24 19:53:41 +0600

    home screen updated

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -195,28 +195,105 @@ class HomeScreen extends StatelessWidget {
                   )),
               child: ListView(
                 children: [
+
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2.5,
-                        height: MediaQuery.of(context).size.height / 6,
-                        decoration: BoxDecoration(
-                          color: kPrimaryColor,
-                          borderRadius:
-                              BorderRadius.circular(kDefaultPadding / 2),
-                        ),
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              'assets/icons/ask.png',
-                              height: 40,
-                              width: 40,
-                            ),
-                          ],
-                        ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/quiz.svg',
+                        title: 'Quiz',
                       ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/assignment.svg',
+                        title: 'Assignments',
+                      ),
+
                     ],
-                  )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/datasheet.svg',
+                        title: 'Data',
+                      ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/calender.svg',
+                        title: 'Holidays',
+                      ),
+
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/results.svg',
+                        title: 'Results',
+                      ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/time.svg',
+                        title: 'Time',
+                      ),
+
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/gallery.svg',
+                        title: 'Gallery',
+                      ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/event.svg',
+                        title: 'Events',
+                      ),
+
+                    ],
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/ask.svg',
+                        title: 'Ask',
+                      ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/password.svg',
+                        title: 'Change\nPassword',
+                      ),
+
+                    ],
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/application.svg',
+                        title: 'Leave\nApplication',
+                      ),
+                      HomeCard(
+                        onPress: (){},
+                        icon: 'assets/icons/logout.svg',
+                        title: 'Logout',
+                      ),
+
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -224,5 +301,52 @@ class HomeScreen extends StatelessWidget {
         ),
       ],
     ));
+  }
+}
+
+
+class HomeCard extends StatelessWidget {
+  const HomeCard({Key? key, required this.onPress, required this.icon, required this.title}) : super(key: key);
+  final VoidCallback onPress;
+  final String icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.only(top: kDefaultPadding/2),
+        width: MediaQuery.of(context).size.width / 2.5,
+        height: MediaQuery.of(context).size.height / 6,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius:
+          BorderRadius.circular(kDefaultPadding / 2),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: kDefaultPadding,
+            ),
+            SvgPicture.asset(
+              icon,
+              height: 40,
+              width: 40,
+              color: kOtherColor,
+            ),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+            const SizedBox(
+              height: kDefaultPadding / 3,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
